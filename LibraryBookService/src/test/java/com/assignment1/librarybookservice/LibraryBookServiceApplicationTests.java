89acada0c5d8012b6ae6b59935e693765095629f
William <william.chalifoux@gmail.com>
2022-09-05 13:50:45 -0400

    Initial project
    Start of the project

---
+++ LibraryBookService/src/test/java/com/assignment1/librarybookservice/LibraryBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment1.librarybookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBookServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
